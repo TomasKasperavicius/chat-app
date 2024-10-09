ae816a4998e0a93d71a85682824b955904c12813
Tomas Kasperavicius <tomkasper01@gmail.com>
2024-10-09 22:37:14 +0300

    disallow duplicate registrations

--- server/Routes/authRouter.ts
+++ server/Routes/authRouter.ts
@@ -8,15 +8,22 @@ const router = express.Router();
 router.post("/register", async (req: Request, res: Response) => {
   try {
     const { avatar, username, password, email }: UserInfo = req.body;
-    const hashedPassword = await argon2.hash(password);
-    const user = new User({
-      avatar,
-      username,
-      password: hashedPassword,
-      email,
-    });
-    await user.save();
-    res.status(200).json(user);
+    const user = User.findOne({username:username, email:email})
+    if(!user)
+      {
+      const hashedPassword = await argon2.hash(password);
+      const newUser = new User({
+        avatar,
+        username,
+        password: hashedPassword,
+        email,
+      });
+      await newUser.save();
+      res.status(200).json(user);
+    }
+    else{
+      res.status(409).json({error:"User already exists."})
+    }
   } catch (error) {
     res.status(500).json(error);
   }
@@ -26,8 +33,11 @@ router.post("/login", async (req: Request, res: Response) => {
   try {
     const { username, password }: UserInfo = req.body;
     const user = await User.findOne({ username });
-    if (!user || !(await argon2.verify(user.password as string, password))) {
-      return res.status(403).json({ error: "Invalid credentials" });
+    if (!user) {
+      return res.status(403).json({ error: "Wrong email." });
+    }
+    if (!(await argon2.verify(user.password as string, password))) {
+      return res.status(403).json({ error: "Wrong password." });
     }
     res.status(200).json(user);
   } catch (error) {
